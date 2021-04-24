d9bb4545eb0c1f8b88c230b991a2f1fe85e96a77
Diogo Broco <dpintobroco@gmail.com>
2021-04-24 15:57:49 +0100

    New restaurant app created with little css or zero

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  { name: 'Dragon Fly', address: 'Chinatown', category: 'chinese', phone_number: '34950093402394' },
+  { name: 'Pizza East', address: 'Shoreditch', category: 'italian', phone_number: '312121341' },
+  { name: 'Tokyo Ramen', address: 'Brixton', category: 'japanese', phone_number: '349233493402394' },
+  { name: 'Hollywood escargot', address: 'Chelsey', category: 'french', phone_number: '34234324' },
+  { name: 'Vegan House', address: 'Clapham Junction', category: 'belgian', phone_number: '324234525' }
+]
+Restaurant.create(restaurants_attributes)
+puts 'Finished!'
